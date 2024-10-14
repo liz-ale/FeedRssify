d5800dfcbcba47a7816e90e47c6e7f0d3127201a
Lizbeth Alpizar <lizbeth.alejandro@neoris.com>
2024-10-14 16:01:19 -0600

    Add new project

---
+++ FeedRssfy_DS/FeedRssfy_DS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FeedRssfy_DS
+//
+//  Created by lizbeth.alejandro on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FeedRssfy_DS/FeedRssfy_DS/FeedRssfy_DSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedRssfy_DSApp.swift
+//  FeedRssfy_DS
+//
+//  Created by lizbeth.alejandro on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FeedRssfy_DSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
