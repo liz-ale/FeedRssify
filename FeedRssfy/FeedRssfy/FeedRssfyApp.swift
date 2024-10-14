9a8259808a5badc3d0bc1004654553ff8d9c6963
Lizbeth Alpizar <lizbeth.alejandro@neoris.com>
2024-10-14 14:23:48 -0600

    Initial commit

---
+++ FeedRssfy/FeedRssfy/FeedRssfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedRssfyApp.swift
+//  FeedRssfy
+//
+//  Created by lizbeth.alejandro on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FeedRssfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
